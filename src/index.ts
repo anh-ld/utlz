7b99546b5d7228e41d8f3af8eabb190f703ee484
Anh <ledzanh@gmail.com>
2022-11-30 17:41:38 +0700

    chore: format

--- src/index.ts
+++ src/index.ts
@@ -8,34 +8,34 @@ declare var store: KV.Namespace;
 declare var store_reverse: KV.Namespace;
 
 interface U {
-	url: string
+  url: string;
 }
 
 const API = new Router();
 
 API.add('POST', '/u', async (req, res) => {
-	const body = await req.body<U>();
-	if (!body?.url) return res.send(400, 'Error');
+  const body = await req.body<U>();
+  if (!body?.url) return res.send(400, 'Error');
 
-	let hash = await read(store, body.url, 'text');
-	
-	if (!hash) {
-		hash = toUID(3);
-		await write(store, body.url, hash); // store {url: hash}
-		await write(store_reverse, hash, body.url) // store {hash: url}
-	}
+  let hash = await read(store, body.url, 'text');
 
-	return res.send(200, hash);
+  if (!hash) {
+    hash = toUID(3);
+    await write(store, body.url, hash); // store {url: hash}
+    await write(store_reverse, hash, body.url); // store {hash: url}
+  }
+
+  return res.send(200, hash);
 });
 
-API.add('GET', '/:id',async (req, res) => {
-	const hash = req.params.id;
-	
-	let url = await read(store_reverse, hash, 'text');
-	if (!url) return res.send(404);
+API.add('GET', '/:id', async (req, res) => {
+  const hash = req.params.id;
 
-	res.writeHead(302, {'Location': url});
-	return res.end(null);
+  let url = await read(store_reverse, hash, 'text');
+  if (!url) return res.send(404);
+
+  res.writeHead(302, { Location: url });
+  return res.end(null);
 });
 
 API.add('GET', '/health', (_, res) => res.end('OK'));
